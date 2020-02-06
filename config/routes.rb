ec0ccdcf179c780bfc1d7880bfe5342284631265
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-02-06 14:04:35 -0600

    front end new and create actions/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :client do
     get '/recipes' => 'recipes#index'
+    get '/recipes/new' => 'recipes#new'
+    get '/recipes/:id' => 'recipes#show'
+
+    post '/recipes' => 'recipes#create'
   end
 end
